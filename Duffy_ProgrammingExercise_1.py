49cdbc059293618da9692cf8941c3ddd05f1bffc
mduffy11 <mduffy11@student.scf.edu>
2026-01-21 21:58:57 -0500

    Criteria as comments.

--- Duffy_ProgrammingExercise_1.py
+++ Duffy_ProgrammingExercise_1.py
@@ -1,24 +1,37 @@
-# Write an application to pre-sell a limited number of cinema tickets.
-def ticket_stand():
-    # No more than 20 tickets can be sold total.
-    tickets = 20
-    buyers = 0
-    # Repeat until all tickets have been sold.
-    while tickets > 0:
-        buyers += 1
-        ticket = tickets - buyer()
-    # and then display the number of remaining tickets after their purchase.
-    print(tickets)
+# # Write an application to pre-sell a limited number of cinema tickets.
+# def ticket_stand():
+#     # No more than 20 tickets can be sold total.
+#     tickets = 20
+#     buyers = 0
+#     # Repeat until all tickets have been sold.
+#     while tickets > 0:
+#         buyers += 1
+#         tickets = tickets - buyer()
+#         # and then display the number of remaining tickets after their purchase.
+#         print(tickets)
+#     else:
+#         sales_report()
 
-def buyer():
+# def sales_report(buyers):
+#     # Then display the total number of buyers.
+#     print(buyers)
+
+def buyer(remaining_tickets):
     # Each buyer can buy up to 4 tickets.
     limit = 4
-    print("Hello, welcome to the Ticket Stand!")
+    print('Hello, welcome to the Ticket Stand!')
     # Prompt the user for the desired number of tickets
-    sold = int(input("How many tickets would you like to buy?: "))
-    return sold
+    sold = input('How many tickets would you like to buy?: ')
 
+    while sold.isdigit() == False:
+        sold = input("I'm sorry, can you please tell me how many tickets?: ")
+    else:
+        sold = int(sold)
+        if sold <= limit and sold <= remaining_tickets:
+             print(f'Alright, here are {sold} tickets!')
+             return sold
+        else:
+            print('Sorry, we do not have enough tickets!')
+    # return sold
 
-def sales_report(buyers):
-    # Then display the total number of buyers.
-    print(buyers)
+buyer(20)
