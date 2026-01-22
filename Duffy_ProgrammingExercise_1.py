bdd21bb9f9ca0f56682779bf4573555dbcee0bbb
mduffy11 <mduffy11@student.scf.edu>
2026-01-21 23:20:35 -0500

    It is fully functional.

--- Duffy_ProgrammingExercise_1.py
+++ Duffy_ProgrammingExercise_1.py
@@ -1,20 +1,16 @@
-# # Write an application to pre-sell a limited number of cinema tickets.
-# def ticket_stand():
-#     # No more than 20 tickets can be sold total.
-#     tickets = 20
-#     buyers = 0
-#     # Repeat until all tickets have been sold.
-#     while tickets > 0:
-#         buyers += 1
-#         tickets = tickets - buyer()
-#         # and then display the number of remaining tickets after their purchase.
-#         print(tickets)
-#     else:
-#         sales_report()
-
-# def sales_report(buyers):
-#     # Then display the total number of buyers.
-#     print(buyers)
+# Write an application to pre-sell a limited number of cinema tickets.
+def ticket_stand():
+    # No more than 20 tickets can be sold total.
+    tickets = 20
+    buyers = 0
+    # Repeat until all tickets have been sold.
+    while tickets > 0:
+        tickets = tickets - buyer(tickets)
+        buyers += 1
+        # and then display the number of remaining tickets after their purchase.
+        print(f'Enjoy the show! We have {tickets} tickets left!')
+    # Then display the total number of buyers.
+    print(f'(We had {buyers} buyers buy tickets)')
 
 def buyer(remaining_tickets):
     # Each buyer can buy up to 4 tickets.
@@ -37,4 +33,4 @@ def buyer(remaining_tickets):
         else:
             print('Sorry, we do not have enough tickets!')
 
-buyer(20)
+ticket_stand()
