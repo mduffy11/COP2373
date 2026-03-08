32bb9f46b391f21a016163d161e3c098e74e0661
mduffy11 <mduffy11@student.scf.edu>
2026-03-08 18:50:42 -0400

    Exercise 6 Initial Commit!

---
+++ Duffy_ProgrammingExercise_6.py
@@ -0,0 +1,35 @@
+def main():
+    print("Welcome To The Personal Information Validator!")
+    print("Please enter your information to be validated.\n")
+
+    # Get user input and strip leading/trailing whitespace
+    phone_input = input("Enter your phone number: ").strip()
+    ssn_input = input("Enter your Social Security Number: ").strip()
+    zip_input = input("Enter your ZIP code: ").strip()
+
+    # Call validation functions
+    phone_checked = validate_phone(phone_input)
+    ssn_checked = validate_ssn(ssn_input)
+    zip_valid = validate_zip(zip_input)
+
+    # Print the results of the validation functions
+    print("\n Validation Results:")
+
+    if phone_checked:
+        print("The phone number you entered is valid.")
+    else:
+        print("The phone number you entered is invalid.")
+
+    if ssn_checked:
+        print("The social security number you entered is valid.")
+    else:
+        print("The social security number you entered is invalid.")
+
+    if zip_valid:
+        print("The ZIP code you entered is valid.")
+    else:
+        print("The ZIP code you entered is invalid.")
+
+# Call main() to execute the program
+if __name__ == "__main__":
+    main()
