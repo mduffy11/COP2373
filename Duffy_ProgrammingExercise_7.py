990d74780e7cd9507abaf3eb85eeab5d11f63299
mduffy11 <mduffy11@student.scf.edu>
2026-03-22 16:43:01 -0400

    Exercise 7 begins

---
+++ Duffy_ProgrammingExercise_7.py
@@ -0,0 +1,18 @@
+import re
+
+def acceptor():
+    print()
+    print('Welcome to the Sentence Enumeration Utility')
+    print('Please input a paragraph for which you would')
+    print('like to enumerate its sentences below.')
+
+    paragraph = input('> ')
+
+    sentence_count, sentence_list = enumerator(paragraph)
+
+def enumerator(paragraph):
+
+
+
+
+acceptor()
