47171623d39f3a82f945e7ae91591e423af1f1d6
mduffy11 <mduffy11@student.scf.edu>
2026-03-22 18:10:54 -0400

    Exercise 7 enumerator

--- Duffy_ProgrammingExercise_7.py
+++ Duffy_ProgrammingExercise_7.py
@@ -10,9 +10,18 @@ def acceptor():
 
     sentence_count, sentence_list = enumerator(paragraph)
 
-#def enumerator(paragraph):
+def enumerator(paragraph):
 
+    pattern = r'[A-Z0-9].*?[.!?](?= [A-Z0-9]|$)'
 
+    # Match all valid sentences in the paragraph.
+    sentence_list = re.findall(pattern, paragraph, flags=re.DOTALL | re.MULTILINE)
+
+    # Count the total number of matched sentences.
+    sentence_count = len(sentence_list)
+
+    # Return the sentence count and the list of matched sentences.
+    return sentence_count, sentence_list
 
 
 acceptor()
